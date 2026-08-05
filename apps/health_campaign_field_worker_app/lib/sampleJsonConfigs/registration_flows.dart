055e98791910b301add85e589b5f1983cccb8667
sachinsaini <sachin.saini@egov.global>
2026-08-05 17:06:44 +0530

    fix: itn count issue fixed

--- apps/health_campaign_field_worker_app/lib/sampleJsonConfigs/registration_flows.dart
+++ apps/health_campaign_field_worker_app/lib/sampleJsonConfigs/registration_flows.dart
@@ -437,7 +437,7 @@ final dynamic sampleFlows = {
                 {
                   "key": "NUMBER_OF_ITN_FOR_DELIVERY",
                   "value":
-                      "{{contextData.0.eligibleProductVariants.0.ProductVariants.0.quantity}}",
+                      "{{contextData.0.targetCycle.0.deliveries.0.doseCriteria.0.ProductVariants.0.quantity}}",
                   "isActive": true
                 }
               ],
@@ -587,7 +587,7 @@ final dynamic sampleFlows = {
                       {
                         "key": "qty",
                         "value":
-                            "{{contextData.0.eligibleProductVariants.0.ProductVariants.0.quantity}}"
+                            "{{contextData.0.targetCycle.0.deliveries.0.doseCriteria.0.ProductVariants.0.quantity}}"
                       },
                       {
                         "key": "nameOfIndividual",
@@ -942,7 +942,7 @@ final dynamic sampleFlows = {
                 {
                   "key": "NUMBER_OF_ITN_FOR_DELIVERY",
                   "value":
-                      "{{contextData.0.eligibleProductVariants.0.ProductVariants.0.quantity}}",
+                      "{{contextData.0.targetCycle.0.deliveries.0.doseCriteria.0.ProductVariants.0.quantity}}",
                   "isActive": true
                 }
               ],
